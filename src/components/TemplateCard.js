bb18e0a62f40dedc3dc51e2b2d144e0968d02b19
nazorm <nnadoziechinazor@gmail.com>
2021-05-27 23:39:45 +0100

    Template Card

---
+++ src/components/TemplateCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TemplateCard = () => {
+  return (
+    <div className="template-card">
+      <h1>One</h1>
+    </div>
+  );
+};
+
+export default TemplateCard;
