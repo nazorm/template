8f5635ca0cac3b510d31fe3a73ea8ff0a9580a6b
nazorm <nnadoziechinazor@gmail.com>
2021-05-29 23:24:30 +0100

    App

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import infoIcon from "./components/assets/info-icon.svg";
 import Filter from "./components/Filter";
-import TemplateCard from "./components/TemplateCard"
+import TemplateCard from "./components/TemplateCard";
+import Pagination from "./components/Pagination";
 import "./App.scss";
 
-
 function App() {
+  const [templateList, setTemplateList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [templatesPerPage, setTemplatesPerPage] = useState(20);
 
-  const [templateList, setTemplateList] = useState([])
-  const [loading, setLoading] = useState(true)
+  // fetch templates
+  useEffect(() => {
+    const fetchTemplates = async () => {
+      const res = await axios.get(
+        "https://front-end-task-dot-fpls-dev.uc.r.appspot.com/api/v1/public/task_templates"
+      );
+      setTemplateList(res.data);
+      setLoading(false);
+      console.log(res.data);
+    };
+    fetchTemplates();
+  }, []);
 
 
-useEffect(()=>{
-  const fetchTemplates = async ()=>{
- const res = await axios.get(
-   "https://front-end-task-dot-fpls-dev.uc.r.appspot.com/api/v1/public/task_templates"
- )
- console.log(res.data)
-  }
-  fetchTemplates()
-},[])
+  //Get current Posts
+  const indexOfLastTemplate = currentPage * templatesPerPage;
+  const indexOfFirstTemplate = indexOfLastTemplate - templatesPerPage;
+  const CurrentList = templateList.slice(
+    indexOfFirstTemplate,
+    indexOfLastTemplate
+  );
+
+
+  // handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <section className="App">
@@ -35,11 +53,32 @@ useEffect(()=>{
       <div className="template-container">
         <div className="templatecontainer-header">
           <h4 className="templatecontainer-headertext">All Templates</h4>
-          <span className="numberof-templates">templates</span>
+          <span className="numberof-templates">
+            {templateList.length} templates
+          </span>
         </div>
-        <TemplateCard/>
+        {loading ? (
+          <p>Loading, please wait...</p>
+        ) : (
+          <div className="template">
+            {CurrentList.map((template) => {
+              return <TemplateCard 
+              key={template.created}
+              template = {template}
+               />;
+            })}
+          </div>
+        )}
       </div>
-     
+      {loading ? (
+        ""
+      ) : (
+        <Pagination
+          templatesPerPage={templatesPerPage}
+          totaltemplates={templateList.length}
+          handlePageChange={handlePageChange}
+        />
+      )}
     </section>
   );
 }
