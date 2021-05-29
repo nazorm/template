0c7d0eaaf46482afb90c16f43f8ebafb4ce7a20d
nazorm <nnadoziechinazor@gmail.com>
2021-05-29 23:24:46 +0100

    Template Card

--- src/components/TemplateCard.js
+++ src/components/TemplateCard.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const TemplateCard = () => {
+const TemplateCard = ({template}) => {
+  const {name, created, category, description, link} = template;
   return (
     <div className="template-card">
-      <h1>One</h1>
+      <h3 className="name">{name}</h3>
+      <span className="category">{category}</span>
+      <p className="description">{description}</p>
+      <span>{created}</span>
+      <a href={link} target="_blank" rel="noopener noreferrer" className="template-cta">Use Template</a>
     </div>
   );
 };
