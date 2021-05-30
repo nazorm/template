ff4c0d7b2d392a31fe5040db6921ae569558ba01
nazorm <nnadoziechinazor@gmail.com>
2021-05-30 23:39:01 +0100

    Filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,29 +1,33 @@
 import React from "react";
-import "../App.scss"
+import "../App.scss";
 import searchIcon from "./assets/search-icon.svg";
 
 const Filter = () => {
   return (
     <div className="search-heading">
-        <div className="searchbar-container">
-        <input type="text" placeholder="Search Templates" className="searchbar" />
-        <img src={searchIcon} alt="search" className="search-icon"/>
-        </div>
-     
+      <div className="searchbar-container">
+        <input
+          type="text"
+          placeholder="Search Templates"
+          className="searchbar"
+        />
+        <img src={searchIcon} alt="search" className="search-icon" />
+      </div>
+
       <div className="filters-sect">
         <span className="label">Sort By:</span>
-          <select className="filter">
-              <option name="all">All</option>
-              <option name="agriculture">Agriculture</option>
-          </select>
+        <select className="filter">
+          <option name="all">All</option>
+          <option name="agriculture">Agriculture</option>
+        </select>
 
-          <select className="filter">
-              <option name="default">Default</option>
-          </select>
+        <select className="filter">
+          <option name="default">Default</option>
+        </select>
 
-          <select className="filter">
-              <option name="default">Default</option>
-          </select>
+        <select className="filter">
+          <option name="default">Default</option>
+        </select>
       </div>
     </div>
   );
