2dc14c3479b0237f28f796e97b8644c3a04e7d3f
nazorm <nnadoziechinazor@gmail.com>
2021-05-30 22:22:53 +0100

    Template Card

--- src/components/TemplateCard.js
+++ src/components/TemplateCard.js
@@ -1,14 +1,19 @@
 import React from "react";
+import "../App.scss"
 
 const TemplateCard = ({template}) => {
   const {name, created, category, description, link} = template;
+  const date = new Date(created).toLocaleDateString()
   return (
     <div className="template-card">
-      <h3 className="name">{name}</h3>
+      <h3 className="name">{name} form template</h3>
       <span className="category">{category}</span>
       <p className="description">{description}</p>
-      <span>{created}</span>
+      <span className="date-created">{date}</span>
+      <div className="cta-container">
       <a href={link} target="_blank" rel="noopener noreferrer" className="template-cta">Use Template</a>
+      </div>
+     
     </div>
   );
 };
