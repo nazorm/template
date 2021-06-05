ad2cd2adb6c2f48da3ee0d6c76c2677f9eeac7a3
nazorm <nnadoziechinazor@gmail.com>
2021-06-05 22:36:54 +0100

    Filter names

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../App.scss";
 import searchIcon from "./assets/search-icon.svg";
 
-const Filter = () => {
+const Filter = ({handleOrder, handleDate}) => {
   return (
     <div className="header">
       <div className="searchbar-container">
@@ -16,18 +16,30 @@ const Filter = () => {
 
       <div className="filters-sect">
         <span className="label">Sort By:</span>
-        <select className="filter">
-          <option name="all">All</option>
-          <option name="agriculture">Agriculture</option>
-        </select>
+        <div className="filter-container">
+          <span className="select-label">Category</span>
+          <select className="filter">
+            <option name="all">All</option>
+            <option name="agriculture">Agriculture</option>
+          </select>
+        </div>
 
-        <select className="filter">
-          <option name="default">Default</option>
-        </select>
-
-        <select className="filter">
-          <option name="default">Default</option>
-        </select>
+        <div className="filter-container">
+          <span className="select-label">Order</span>
+          <select className="filter" onChange={handleOrder}>
+            <option name="default">Default</option>
+            <option name="ascending">Ascending</option>
+            <option name="descending">Descending</option>
+          </select>
+        </div>
+        <div className="filter-container">
+          <span className="select-label">Date</span>
+          <select className="filter" onChange={handleDate}>
+            <option name="default">Default</option>
+            <option name="ascending">Ascending</option>
+            <option name="descending">Descending</option>
+          </select>
+        </div>
       </div>
     </div>
   );
