dfb20733fa2efabd0e134629655a25715003875d
nazorm <nnadoziechinazor@gmail.com>
2021-06-05 22:36:34 +0100

    handle order and date sort

--- src/App.js
+++ src/App.js
@@ -8,41 +8,88 @@ import "./App.scss";
 
 function App() {
   const [templateList, setTemplateList] = useState([]);
+  const [filteredTemplates, setFilteredTemplates] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
-  const [templatesPerPage, setTemplatesPerPage] = useState(20);
+  const [templatesPerPage] = useState(20);
+  const [filters, setFilters] = useState({ order: "", date: "" });
 
   // fetch templates
-  useEffect(() => {
-    const fetchTemplates = async () => {
-      const res = await axios.get(
-        "https://front-end-task-dot-fpls-dev.uc.r.appspot.com/api/v1/public/task_templates"
-      );
-      setTemplateList(res.data);
-      setLoading(false);
-      console.log(res.data);
-    };
-    fetchTemplates();
-  }, []);
+  const fetchTemplates = async () => {
+    const res = await axios.get(
+      "https://front-end-task-dot-fpls-dev.uc.r.appspot.com/api/v1/public/task_templates"
+    );
+    setTemplateList(res.data);
+    setFilteredTemplates(res.data);
+    setLoading(false);
+    console.log(filteredTemplates);
+  };
 
+  // handle page change
+  const handlePageChange = ({ selected }) => {
+    const page = selected + 1;
+    setCurrentPage(page);
+  };
+
+  //handle filters for date and order
+  const handleFilters = (newFilters) => {
+    let isFilteredTemplates = templateList;
+    if (newFilters.order === "default" && newFilters.date === "default") {
+      isFilteredTemplates = templateList;
+      setFilteredTemplates(isFilteredTemplates);
+      return;
+    }
+    if (newFilters.date) {
+      switch (newFilters.date) {
+        case "ascending":
+          isFilteredTemplates = isFilteredTemplates.sort(
+            (a, b) => a.date - b.date
+          );
+          break;
+        case "descending":
+          isFilteredTemplates = isFilteredTemplates.sort(
+            (a, b) => b.date - a.date
+          );
+          break;
+      }
+    }
+    if (newFilters.order) {
+      isFilteredTemplates = isFilteredTemplates.sort();
+    }
+    setFilteredTemplates(isFilteredTemplates);
+    console.log(filteredTemplates);
+  };
+
+  // handle order change
+  const handleOrder = (e) => {
+    const { value } = e.target;
+    const newFilters = { ...filters, order: value };
+    setFilters(newFilters);
+    handleFilters(newFilters);
+  };
+  //handle date change
+  const handleDate = (e) => {
+    const { value } = e.target;
+    const newFilters = { ...filters, date: value };
+    setFilters(newFilters);
+    handleFilters(newFilters);
+  };
 
   //Get current Posts
   const indexOfLastTemplate = currentPage * templatesPerPage;
   const indexOfFirstTemplate = indexOfLastTemplate - templatesPerPage;
-  const CurrentList = templateList.slice(
+  const CurrentList = filteredTemplates.slice(
     indexOfFirstTemplate,
     indexOfLastTemplate
   );
-
-
-  // handle page change
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+  // useEffect
+  useEffect(() => {
+    fetchTemplates();
+  }, []);
 
   return (
     <section className="App">
-      <Filter />
+      <Filter handleOrder={handleOrder} handleDate={handleDate} />
       <div className="freetemplate-info">
         <img src={infoIcon} alt="info" className="info-icon" />
         <span className="info-text">
@@ -54,7 +101,7 @@ function App() {
         <div className="templatecontainer-header">
           <h4 className="templatecontainer-headertext">All Templates</h4>
           <span className="numberof-templates">
-            {loading? " " : templateList.length} templates
+            {loading ? " " : filteredTemplates.length} templates
           </span>
         </div>
         {loading ? (
@@ -62,10 +109,9 @@ function App() {
         ) : (
           <div className="templates">
             {CurrentList.map((template) => {
-              return <TemplateCard 
-              key={template.created}
-              template = {template}
-               />;
+              return (
+                <TemplateCard key={template.created} template={template} />
+              );
             })}
           </div>
         )}
