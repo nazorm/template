51b38a6683222ec1d2d096c82f9bcf12fa8584ad
nazorm <nnadoziechinazor@gmail.com>
2021-06-06 09:54:26 +0100

    Clean up pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactPaginate from "react-paginate";
 const Pagination = ({ templatesPerPage, totaltemplates, handlePageChange }) => {
-  const pageNumbers = [];
-
   const displayPage = Math.ceil(totaltemplates / templatesPerPage);
-  for (let i = 1; i <= displayPage; i++) {
-    pageNumbers.push(i);
-  }
 
   return (
     <div className="pages-container">
